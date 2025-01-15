b608a819b14bf1a9a7ee0e48574611e833ee3dfb
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-15 18:34:35 -0500

    thread execution by creating obj of same class

---
+++ Multiprogramming/MyThread1.java
@@ -0,0 +1,25 @@
+package Multiprogramming;
+
+public class MyThread1 extends Thread
+{
+    public void run()
+    {
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+" Hello");
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread1 t=new MyThread1();
+        t.start();
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+" World");
+            i++;
+        }
+    }
+}
