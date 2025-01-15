04a3aa80ab37ffb30abf0da5d33bbd9327b824a2
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-15 18:33:16 -0500

    thread execution by creating obj of diff. class
    Execution of multithreading by creating object of a class which is extending from the Thread class.

---
+++ Multiprogramming/MyThread.java
@@ -0,0 +1,30 @@
+package Multiprogramming;
+
+
+class MyThreadClass extends Thread
+{
+    public void run()
+    {
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+" Hello");
+            i++;
+        }
+    }
+}
+public class MyThread
+{
+    public static void main(String[] args) {
+        MyThreadClass m=new MyThreadClass();
+        m.start();
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+" World");
+        i++;
+        }
+    }
+
+
+}
