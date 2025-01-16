32acc6d7c1264dc26dc8d1ac2b3c4ec9f56ba0f5
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-16 11:56:12 -0500

    thread execution using Runnable interface part 1

---
+++ Multiprogramming/Test.java
@@ -0,0 +1,30 @@
+package Multiprogramming;
+
+
+class My implements Runnable
+{
+    public void run()
+    {
+        int i=1;
+        while(true){
+            System.out.println(i+" Hello");
+            i++;
+        }
+    }
+}
+public class Test
+{
+    public static void main(String[] args) {
+
+        My m=new My();
+        Thread t=new Thread(m); //imp line
+        t.start();
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+" World");
+            i++;
+        }
+
+    }
+}
