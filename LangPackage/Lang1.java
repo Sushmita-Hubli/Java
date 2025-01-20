d279e25964016f61f09a54e0ea4d35a0b50526e7
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-20 12:09:50 -0500

    Object class of lang package

---
+++ LangPackage/Lang1.java
@@ -0,0 +1,12 @@
+package LangPackage;
+
+public class Lang1
+{
+    public static void main(String[] args) {
+        System.out.println("Super class of all the classes");
+        Object o1=new Object();
+        System.out.println(o1.toString());
+        System.out.println(o1.hashCode());
+
+    }
+}
